3c10a59da721b069e330df6bffbe7a2e0a0b0cfe
NewbySlime <adikafirja@gmail.com>
2024-06-08 23:17:14 +0700

    Added more Comments

--- Assets/SourceCode/MiniGameHandler.cs
+++ Assets/SourceCode/MiniGameHandler.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 // Komponen penting ketika akan melakukan "MiniGame" yang dimana pemakaiannya cukup luas. Seperti contoh, pada scene "base_level" ada dua "ekstensi" dari MiniGameHandler yang dipakai. Contohnya, Komponen EnemyFightMG dan BossFightMG.
 // Komponen ini juga memberikan fungsi yang bisa dipakai sebagai ekstensi dari objek ini, seperti contoh memberikan fungsi menjalankan Sequence ketika ResultCase tercapai.
 public class MiniGameHandler: MonoBehaviour{
+  // Enum yang dipakai untuk menentukan apa ResultCase dari akhir MiniGame tersebut.
   public enum ResultCase{
     Win,
     Lose
   }
 
+  // Ini Sequence data yang digunakan ketika ResultCase tercapai.
   public struct ResultSequenceData{
     public ResultCase Case;
     public SequenceHandlerVS SequenceHandler;
@@ -42,8 +44,11 @@ public class MiniGameHandler: MonoBehaviour{
     _result_sequence_map[result] = _sequence_handler;
   }
 
+  // PENTING: ini adalah fungsi yang dipakai sebagai Callback untuk ekstensi kelas.
   protected virtual void _OnGameFinished(ResultCase result){}
 
+
+  // PENTING: ini fungsi yang dipakai ketika komponen ekstensi memberi tahu bahwa "Game ini sudah selesai, dan hasilnya: X".
   protected void _GameFinished(ResultCase result){
     Debug.Log("mini game finished");
     if(!IsMiniGameRunning)
@@ -73,10 +78,13 @@ public class MiniGameHandler: MonoBehaviour{
   }
 
 
+  // Fungsi yang dipakai di Visual Scripting untuk menambahakan Sequence saat terjadi setelah MiniGame selesai dengan ResultCase tertentu.
   public void SetResultCaseSequence(ResultCase result, SequenceHandlerVS.SequenceInitializeData data){
     StartCoroutine(_set_result_case_sequence(result, data));
   }
 
+  
+  // PENTING: Funsgi yang dipakai oleh MiniGame ataupun komponen ekstensi ketika ada objek atau actor yang menjalankan/mentrigger MiniGame ini.
   public virtual void TriggerGameStart(){
     Debug.Log("mini game started");
     IsMiniGameRunning = true;
